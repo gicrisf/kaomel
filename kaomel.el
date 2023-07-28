23e316d212b8a7b15c618a74b22836c376728780
Giovanni Crisalfi <giovanni.crisalfi@protonmail.com>
2023-07-28 15:07:49 +0200

    More options, more control.

--- kaomel.el
+++ kaomel.el
@@ -5,8 +5,8 @@
 ;; Author: Giovanni Crisalfi <giovanni.crisalfi@protonmail.com>
 ;; Maintainer: Giovanni Crisalfi <giovanni.crisalfi@protonmail.com>
 ;; Created: luglio 19, 2023
-;; Modified: luglio 27, 2023
-;; Version: 0.0.3
+;; Modified: luglio 28, 2023
+;; Version: 0.0.4
 ;; Keywords: convenience extensions faces tools
 ;; Homepage: https://github.com/gicrisf/kaomel
 ;; Package-Requires: ((emacs "27.1"))
@@ -37,17 +37,75 @@
   :group 'kaomel
   :type 'string)
 
-(defcustom kaomel-helm nil
-  "To use or not to use helm as completion system.
-Default is NIL, which means we use completion-read instead;
-completion-read can be coupled with Vertico/Selectrum or similar packages."
+(defcustom kaomel-avoid-helm nil
+"Use 'completing-read' even if a functional Helm installation is present.
+Normally, Helm users prefer using Helm for all tasks, but a user might have
+both completion systems and want to specify which one to use with Kaomel.
+The default value is NIL, which means we use Helm if it is available
+and 'completing-read' if it is not."
   :group 'kaomel
   :type 'boolean)
 
-(defcustom kaomel-separator "=>"
-  "To use or not to use helm as completion system.
-Default is NIL, which means we use completion-read instead;
-completion-read can be coupled with Vertico/Selectrum or similar packages."
+(defvar kaomel--getter
+  (if kaomel-avoid-helm
+      #'kaomel--get-through-cr
+    (if (locate-library "helm-core")
+        #'kaomel--get-through-helm
+      #'kaomel--get-through-cr))
+  "The getter function actually called.")
+
+(defun kaomel-insert ()
+  "Pick your kaomoji and insert it in the buffer."
+  (interactive)
+  (insert (funcall kaomel--getter)))
+
+(defun kaomel-to-clipboard ()
+  "Pick your kaomoji and store it in your clipboard."
+  (interactive)
+  (kill-new (funcall kaomel--getter)))
+
+
+
+(defcustom kaomel-tag-langs '("orig" "hepburn" "en")
+  "Preferred languages in showed tags;
+You can choose between:
+- Original (orig)
+- Hiragana (hira)
+- Katakana (kana)
+- English (en)
+- Italian (it)."
+  :group 'kaomel
+  :type 'list)
+
+(defcustom kaomel-only-ascii-tags nil
+  "If t, kaomel filters out every non-ascii character from tags."
+  :group 'kaomel
+  :type 'boolean)
+
+(defcustom kaomel-heavy-trim-tags nil
+  "If t, kaomel shows only the first word for every tag token.
+Useful to make the tag strings shorter when you choose many languages."
+  :group 'kaomel
+  :type 'boolean)
+
+(defcustom kaomel-tag-val-separator "=>"
+  "Separator that is put between the tags and the value."
+  :group 'kaomel
+  :type 'string)
+
+(defcustom kaomel-tag-tag-separator " "
+  "Separator that is put between a tag the tag next to it."
+  :group 'kaomel
+  :type 'string)
+
+(defcustom kaomel-prompt "Pick a Kaomoji:"
+  "The prompt line displayed when prompting the user to pick a Kaomoji.
+
+Example usage:
+(setq kaomel-prompt \"Select a Kaomoji:\")
+
+Changing the value of this variable will update the prompt line in all relevant
+interactions within the `kaomel` package."
   :group 'kaomel
   :type 'string)
 
@@ -62,54 +120,6 @@ Then, it returns them as a parsed object."
                     (json-read)))))
     parsed))
 
-(defun kaomel--get ()
-  "Prompt the user to select a kaomoji using 'completing-read'."
-  (let ((completions (kaomel--get-candidates)))
-    (cdr (assoc (completing-read "Pick a kaomoji: " completions)
-                 completions))))
-
-(defun kaomel--get-candidates ()
-  "Get candidates as alist.
-\(DISPLAY . REAL\)"
-  (mapcar (lambda (moji-el)
-            (cons (concat (car moji-el)
-                          " "
-                          kaomel-separator
-                          " "
-                          (cdr moji-el))
-                  (cdr moji-el)))
-          (kaomel--build-general-seq)))
-
-(defun kaomel-insert ()
-  "Pick your kaomoji and insert it in the buffer."
-  (interactive)
-  (insert (if kaomel-helm (kaomel--get-through-helm)
-            (kaomel--get))))
-
-(defun kaomel--get-through-helm ()
-  "Prompt the user to select a kaomoji using Helm."
-  (helm :sources
-        (helm-build-sync-source "Pick a kaomoji:"
-          :candidates (kaomel--get-candidates)
-          :candidate-number-limit 1000)))
-
-(defun kaomel-to-clipboard ()
-  "Pick your kaomoji and store it in your clipboard."
-  (interactive)
-  (kill-new (if kaomel-helm (kaomel--get-through-helm)
-            (kaomel--get))))
-
-(defcustom kaomel-tag-langs '("en" "hepburn")
-  "Preferred languages in showed tags;
-You can choose between:
-- Original (orig)
-- Hiragana (hira)
-- Katakana (kana)
-- English (en)
-- Italian (it)."
-  :group 'kaomel
-  :type 'list)
-
 (defun kaomel--build-general-seq ()
   "Build a generic sequence of kaomojis."
   (let ((agnostic-kaomoji-seq '())
@@ -117,26 +127,64 @@ You can choose between:
     (mapc (lambda (kaomoji-tagged-cluster)
             (let ((tag (mapconcat
                         'identity
+                        ;; no duplicated elements
                         (delete-dups
+                         ;; no nil elements
                          (flatten-tree
                           (mapcar
                            (lambda (el-tag)
+                             ;; only elements from the chosen language
                              (mapcar
                               (lambda (lang)
                                 (let ((word (gethash lang el-tag)))
+                                  ;; only non-white elements
                                   (when (not (string-match-p "^[[:space:]]*$" word))
-                                    (car (split-string
-                                          (string-trim
-                                           (replace-regexp-in-string "[[:nonascii:]]" "" word)))))))
+                                   (let* ((filtered-word
+                                           (if kaomel-only-ascii-tags
+                                               ;; only ascii characters (filter out asian words)
+                                               (replace-regexp-in-string "[[:nonascii:]]" "" word)
+                                             word))
+                                          ;; no white characters around the words
+                                          (filtered-word (string-trim filtered-word))
+                                          (filtered-word
+                                           (if kaomel-heavy-trim-tags
+                                               ;; only the first words of every translation
+                                               (car (split-string filtered-word))
+                                             filtered-word)))
+                                     filtered-word))))
+                              ;; list of languages
                               kaomel-tag-langs))
                            (gethash "tag" kaomoji-tagged-cluster))))
-                        " "))
+                        kaomel-tag-tag-separator))
                   (yan (gethash "yan" kaomoji-tagged-cluster)))
               (setq agnostic-kaomoji-seq
                     (append (mapcar (lambda (moji) (cons tag moji)) yan)
                             agnostic-kaomoji-seq))))
           parsed-json-vec)
     agnostic-kaomoji-seq))
+
+(defun kaomel--get-candidates ()
+  "Get candidates as alist.
+\(DISPLAY . REAL\)"
+  (mapcar (lambda (moji-el)
+            (cons (concat (car moji-el)
+                          " "
+                          kaomel-tag-val-separator
+                          " "
+                          (cdr moji-el))
+                  (cdr moji-el)))
+          (kaomel--build-general-seq)))
+
+(defun kaomel--get-through-cr ()
+  "Prompt the user to select a kaomoji using 'completing-read'."
+  (let ((completions (kaomel--get-candidates)))
+    (cdr (assoc (completing-read
+                 (concat kaomel-prompt " ") completions)
+                completions))))
+
+
+
+
 
 (provide 'kaomel)
 ;;; kaomel.el ends here
